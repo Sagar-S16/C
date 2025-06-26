849f4f4bc5d4971ef2cef830587a84a136bf46d4
Sagar-S16 <sagarcheeku18@gmail.com>
2025-06-26 12:09:33 +0000

    datatypes

---
+++ C.programming/datatypes.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+    int age;
+    printf("enter age:");
+    scanf("%d",&age);
+    printf("%d\n",age);
+    char star = '*';
+    int number = 25;
+    float pi = 3.14;
+    int final_price = 100;
+    printf("%d\n",final_price);
+    printf("%f\n",pi);
+    printf("%c",star);
+ }
+
+    //constant types
+    //types
+    //1.integer constants eg.1,2,4,-1,-2
+    //2.Real constants eg.1.0,2.0,3.14,-2.4
+    //3.character constants eg.'a','b','c','#','&','A'
+
+
+     //32 keywords - that have special meaning to the compiler
+     //comments // /*...../*
+     //output printf("hello world\n")
+     //printf("%d",age);
+     //printf("%f",pi);
+     //printf("%c",star);
+     //%d,%f,%c -- format specifiers
+
+
+     //inputs
+
+     //scanf("%d",&age);
+
+
