2e7f62be2233315dd71d4d4f47be70c87bb6c399
Sagar-S16 <sagarcheeku18@gmail.com>
2025-06-26 12:23:14 +0000

    addition of 2 numbers

---
+++ C.programming/1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//addition of 2 numbers
+int main(){
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+    printf("enter b");
+    scanf("%d",&b);
+    int result = a + b;
+    printf("result : %d",result);
+ }
