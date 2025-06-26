0a5931cff7dec2263647a0f62638dd19575c3f41
Sagar-S16 <sagarcheeku18@gmail.com>
2025-06-26 10:55:54 +0000

    variable

---
+++ C.programming/variable.c
@@ -0,0 +1,15 @@
+//variable is the name of the memory location which stpres some data.
+#include<stdio.h>
+
+int main(){
+    char star = '*';
+    int number = 25;
+    float pi = 3.14;
+    int final_price = 100;
+    printf("%d",final_price);
+    }
+
+    //variables are case sensitive
+    //1st character is alphabet or '_'
+    //no comma or blankspace
+    //no other symbol other than '_'
